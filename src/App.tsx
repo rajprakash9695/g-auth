9a23e9b39cafd1d367b0dbd858da8044525a47f7
Ashutosh Maurya <ashutoshm9695@gmail.com>
2024-05-07 19:27:58 +0530

    chat worked

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,22 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import { AuthProvider } from './context/auth.context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { socket } from './utils/socket';
 
 const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('log: connected', socket.id);
+    });
+
+    return () => {
+      socket.off('connect');
+    };
+  }, []);
+
   return (
     <div>
       <QueryClientProvider client={queryClient}>
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,13 +1,20 @@
 import useAuth from '../hooks/useAuth';
+import { socket } from '../utils/socket';
 
 export default function User() {
   const { user } = useAuth();
 
   console.log('log: user', user);
 
+  function sendMessage() {
+    socket.emit('chat:create', { ...user });
+  }
+
   return (
     <>
       <h1>User</h1>
+
+      <button onClick={sendMessage}>send message</button>
     </>
   );
 }
